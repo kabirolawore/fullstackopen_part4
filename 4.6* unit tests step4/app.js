7494b8fa6dbf6029fb47db5eaf535d36e0613bde
Kabir Olawore <horlaworey@gmail.com>
2023-01-28 16:36:17 +0100

    Added 4.6 project

---
+++ 4.6* unit tests step4/app.js
@@ -0,0 +1,28 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info('Connected to MongoDB'))
+  .catch((error) =>
+    logger.error('error connecting to MongoDB:', error.message)
+  );
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
---
+++ 4.6* unit tests step4/utils/list_helper.js
@@ -0,0 +1,53 @@
+const _ = require('lodash');
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogList) => {
+  return blogList.reduce((acc, next) => acc + next, 0);
+};
+
+const favoriteBlog = (blogList) => {
+  //
+  const newblogList = JSON.parse(JSON.stringify(blogList));
+  newblogList.forEach((blog) => {
+    delete blog.url;
+    delete blog._id;
+    delete blog.__v;
+    delete blog.blogs;
+  });
+
+  let maxLike = Math.max(...newblogList.map((item) => item.likes));
+
+  const favBlog = newblogList.filter((obj) => obj.likes === maxLike);
+
+  return favBlog[0];
+};
+
+const mostBlogs = (blogArr) => {
+  //
+  const newblogArr = JSON.parse(JSON.stringify(blogArr));
+  newblogArr.forEach((blog) => {
+    delete blog.url;
+    delete blog._id;
+    delete blog.__v;
+    delete blog.likes;
+    delete blog.title;
+  });
+
+  let blogsCount = newblogArr.map((item) => item.blogs);
+  let blogsMax = _.max(blogsCount);
+
+  let blogFound = _.find(newblogArr, (blog) => blog.blogs === blogsMax);
+
+  // console.log(blogFound);
+  return blogFound;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+};
