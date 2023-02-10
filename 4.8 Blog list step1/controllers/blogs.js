b308a32b75ed8aa37fdefe660973e180425874e9
kabirolawore <horlaworey@gmail.com>
2023-02-10 16:48:34 +0100

    folder updated

--- 4.8 Blog list step1/controllers/blogs.js
+++ 4.8 Blog list step1/controllers/blogs.js
@@ -1,30 +1,40 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (_request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get('/', async (_request, response) => {
+  //
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then((blog) => {
-      if (blog) response.json(blog);
-      else response.status(404).end();
-    })
-    .catch((error) => next(error));
+blogsRouter.get('/:id', async (request, response, next) => {
+  //
+  //
+  try {
+    const blog = await Blog.findById(request.params.id);
+    if (blog) response.json(blog);
+    else response.status(404).end();
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
+  //
   const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => next(error));
+  try {
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  } catch (exception) {
+    next(exception);
+  }
+  // blog
+  //   .save()
+  //   .then((result) => {
+  //     response.status(201).json(result);
+  //   })
+  //   .catch((error) => next(error));
 });
 
 module.exports = blogsRouter;
--- 4.8 Blog list step1/index.js
+++ 4.8 Blog list step1/index.js
@@ -1,9 +1,9 @@
 const app = require('./app'); // the actual express app
-// const http = require('http');
+const http = require('http');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
