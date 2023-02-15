92129d1e6a42b5e181e007cbffe7ca10cbe42287
kabirolawore <horlaworey@gmail.com>
2023-02-15 13:13:14 +0100

    4.15 project updated

--- 4.15 Bloglist expansion step3/controllers/users.js
+++ 4.15 Bloglist expansion step3/controllers/users.js
@@ -2,8 +2,18 @@ const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
 const User = require('../models/user');
 
-//
-usersRouter.post('/', async (request, response) => {
+// get
+usersRouter.get('/', async (request, response, next) => {
+  try {
+    const users = await User.find({});
+    response.json(users);
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+//post
+usersRouter.post('/', async (request, response, next) => {
   const { username, name, password } = request.body;
 
   const saltRounds = 10;
@@ -15,9 +25,12 @@ usersRouter.post('/', async (request, response) => {
     passwordHash,
   });
 
-  const savedUser = await user.save();
-
-  response.status(201).json(savedUser);
+  try {
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = usersRouter;
--- 4.15 Bloglist expansion step3/models/blog.js
+++ 4.15 Bloglist expansion step3/models/blog.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
   title: { type: String, minLength: 5, required: true },
   author: { type: String, minLength: 5, required: true },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
+  // user: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  // },
   url: { type: String, required: true },
   likes: { type: Number, default: 0 },
 });
