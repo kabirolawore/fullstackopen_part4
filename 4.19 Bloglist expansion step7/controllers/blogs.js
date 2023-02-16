710fb819220afd7452ced15bc62cdb66769be9eb
kabirolawore <horlaworey@gmail.com>
2023-02-16 13:13:05 +0100

    4.19 project added 'token POST'

---
+++ 4.19 Bloglist expansion step7/controllers/blogs.js
@@ -0,0 +1,104 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { request } = require('express');
+
+// get all blogs
+blogsRouter.get('/', async (_request, response, next) => {
+  //
+  try {
+    const blogs = await Blog.find({}).populate('user');
+    response.json(blogs);
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+// get blog by id
+blogsRouter.get('/:id', async (request, response, next) => {
+  //
+  try {
+    const blog = await Blog.findById(request.params.id);
+    if (blog) response.json(blog);
+    else response.status(404).end();
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+// get token
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.replace('bearer ', '');
+  }
+  return null;
+};
+
+// post blog
+blogsRouter.post('/', async (request, response, next) => {
+  // const blog = new Blog(request.body);
+  const body = request.body;
+
+  const token = getTokenFrom(request);
+  // console.log(token);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  });
+
+  try {
+    if (body.url && body.title) {
+      const savedBlog = await blog.save();
+      user.blogs = user.blogs.concat(savedBlog._id);
+      await user.save();
+
+      response.status(201).json(savedBlog);
+    } else response.status(400).end();
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+// delete blog
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+// update blog
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+      new: true,
+    });
+    response.status(200).json(updatedBlog);
+  } catch (exception) {
+    next(exception);
+  }
+});
+module.exports = blogsRouter;
